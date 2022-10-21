dc96c428160333053cb7f741a6fd21a3633118d4
Camilla Avila <avilacamilla@outlook.com>
2022-10-21 12:31:53 -0300

    Add development branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <code>Testado projeto.</code>
         </p>
         <a
           className="App-link"
