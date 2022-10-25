63102955599a6707121ed461f15d26e7eeb751d6
Camilla Avila <avilacamilla@outlook.com>
2022-10-25 00:36:33 -0300

    Adicionando header com evento de scroll e informações do footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
+import Header from './components/Header'
 
-function App() {
-  return <div className="App"></div>;
+
+export default () => {
+  const [blackHeader, setBlackHeader] = useState(false)
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
+  return (
+    <div className='page'>
+
+      <Header black={blackHeader} />
+      <div className='lists'>
+
+      </div>
+      
+      <footer>
+        Inspirado na Netflix<br />
+        Dados pegos do site Themoviedb.org <br />
+        Direitos de imagem para Netflix. <br />
+        Feito por Camilla Avila.
+        
+      </footer>
+    </div>
+  )
 }
-
-export default App;
