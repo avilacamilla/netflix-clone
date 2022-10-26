84f17f41ef0bb9d798b4bd4ae491e9b7e8a6bf49
Camilla Avila <avilacamilla@outlook.com>
2022-10-25 23:14:10 -0300

    Fazendo conexão com API - Themoviedb.org

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import Tmdb from './Tmdb'
+
+import FeaturedMovi from './components/FeaturedMovie'
 import Header from './components/Header'
 
 
 export default () => {
   const [blackHeader, setBlackHeader] = useState(false)
+  const [movieList, setMovieList] = useState([])
+  const[eaturedData, setFeaturedData] = useState(null)
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
+
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      console.log(chosenInfo)
+    }
+    loadAll()
+  }, [])
 
   useEffect(() => {
     const scrollListener = () => {
@@ -31,10 +50,9 @@ export default () => {
       </div>
       
       <footer>
-        Inspirado na Netflix<br />
-        Dados pegos do site Themoviedb.org <br />
-        Direitos de imagem para Netflix. <br />
-        Feito por Camilla Avila.
+        Feito por <a href='https://www.linkedin.com/in/avilacamilla'>Camilla Avila</a> <br />
+        <i>Inspiração</i> & <b>Direitos</b> <a href='https://www.netflix.com/br/'>Netflix</a> <br />
+        Dados pegos do site Themoviedb.org
         
       </footer>
     </div>
