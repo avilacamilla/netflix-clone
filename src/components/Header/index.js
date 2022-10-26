a90812d9d1c9e93dccb6995a1608b93a1d61a93f
Camilla Avila <avilacamilla@outlook.com>
2022-10-26 13:42:28 -0300

    Adicionando âncoras na navbar - início, séries, filmes...

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,6 +9,16 @@ export default ({ black }) => {
                     <img src='https://user-images.githubusercontent.com/115038212/197913878-c586aa2e-b902-4800-b68c-469ecda70857.png' alt='Logotipo Devflix' />
                 </a>
             </div>
+            <div className='header-nav'>
+                <ul>
+                    <a href="#"><li>Início</li></a>
+                    <a href="#"><li>Séries</li></a>
+                    <a href="#"><li>Filmes</li></a>
+                    <a href="#"><li>Bombando</li></a>
+                    <a href="#"><li>Minha lista</li></a>
+                    <a href="#"><li>Navegar por idiomas</li></a>
+                </ul>
+            </div>
             <div className="header-user">
                 <img src='https://user-images.githubusercontent.com/115038212/197917713-fd8bc850-91b2-470c-a796-421615e24e7c.png' alt='Avatar usuário Devflix' />
             </div>
