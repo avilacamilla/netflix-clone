78932cfdf78e5a5b5e157bc9357073796beceb47
Camilla Avila <avilacamilla@outlook.com>
2022-10-29 00:53:41 -0300

    Update Index.js

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -1,55 +1,53 @@
-import React from 'react'
-import './FeaturedMovie.css'
+import React from "react";
+import "./FeaturedMovie.css";
 
-export default ({item}) => {
-    let firstDate = new Date(item.first_air_date)
-    let genres = []
+export default function nome({ item }) {
+  let firstDate = new Date(item.first_air_date);
+  let description = item.overview;
+  let genres = [];
 
-    for(let i in item.genres) {
-        genres.push(item.genres[i].name)
-    }
+  if (description.length > 160) {
+    description = description.substring(0, 160) + " ...";
+  }
 
-    let description = item.overview 
+  for (let indice in item.genres) {
+    genres.push(item.genres[indice].name);
+  }
 
-    return (
-        <section className='featured' style={{
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
-        }}>
-            <div className='featured--vertical'>
-                <div className='featured--horizontal'>
-                    <div className="featured--name">
-                        {item.original_name}
-                    </div>
-                    <div className="featured--info">
-                        <div className="featured--points">
-                            {item.vote_average} pontos
-                        </div>
-                        <div className="featured--year">
-                            {firstDate.getFullYear}
-                        </div>
-                        <div className="featured--seasons">
-                            {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}
-                        </div>
-                    </div>
-                    <div className="featured--description">
-                        {description}
-                    </div>
-                    <div className="featured--buttons">
-                        <a href={`/watch/${item.id}`} className='featured--watchbutton'>
-                            ▶ Assistir
-                        </a>
-                        <a href={`/list/add/${item.id}`} className='featured--mylistbutton'>
-                            + Minha Lista
-                        </a>
-                    </div>
-                    <div className="featured--genres">
-                        <strong>Gêneros: </strong>
-                        {genres.join(', ')}
-                    </div>
-                </div>
+  return (
+    <section
+      className="featured"
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+      }}
+    >
+      <div className="featured--vertical">
+        <div className="featured--horizontal">
+          <div className="featured--name">{item.name}</div>
+          <div className="featured--info">
+            <div className="featured--points">{item.vote_average} pontos</div>
+            <div className="featured--year">{firstDate.getFullYear()}</div>
+            <div className="featured--seasons">
+              {item.number_of_seasons} temporada
+              {item.number_of_seasons !== 1 ? "s" : ""}
             </div>
-        </section>
-    )
+          </div>
+          <div className="featured--description">{description}</div>
+          <div className="featured--buttons">
+            <a href={`/watch/${item.id}`} className="featured--watchbutton">
+              ▶ Assistir
+            </a>
+            <a href={`/list/add/${item.id}`} className="featured--mylistbutton">
+              + Minha Lista
+            </a>
+          </div>
+          <div className="featured--genres">
+            <strong>Gêneros: </strong> {genres.join(", ")}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
